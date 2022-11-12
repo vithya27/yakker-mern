955db7835e7602c3f9d905bfb4c167967b1c75d7
vithya27 <vithya.shankar93@hotmail.com>
2022-11-12 22:11:38 +0800

    Logout button is functional

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -8,8 +8,13 @@ import {
   ArrowRightOnRectangleIcon,
 } from "@heroicons/react/24/outline";
 import SidebarCard from "./SidebarCard";
+import { useLogout } from "../hooks/useLogout";
 
 const Sidebar = () => {
+  const { logout } = useLogout();
+  const handleLogout = () => {
+    logout();
+  };
   return (
     <div className="col-span-2 flex flex-col items-center px-4 md:items-start">
       <img
@@ -22,7 +27,11 @@ const Sidebar = () => {
       <SidebarCard Icon={BellAlertIcon} title="Notifications" />
       <SidebarCard Icon={EnvelopeIcon} title="Messages" />
       <SidebarCard Icon={UserIcon} title="Profile" />
-      <SidebarCard Icon={ArrowRightOnRectangleIcon} title="Logout" />
+      <SidebarCard
+        Icon={ArrowRightOnRectangleIcon}
+        title="Logout"
+        onClick={handleLogout}
+      />
     </div>
   );
 };
