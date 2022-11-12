f5c5ea324b4ab88487d49b0d299485317f7d625f
vithya27 <vithya.shankar93@hotmail.com>
2022-11-12 18:18:47 +0800

    Signup API works

--- client/src/hooks/useSignup.js
+++ client/src/hooks/useSignup.js
@@ -11,6 +11,7 @@ export const useSignup = () => {
 
     const response = await fetch("http://127.0.0.1:5001/users/create", {
       method: "PUT",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
     });
     const json = await response.json();
